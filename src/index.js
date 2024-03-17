629a09232bf23d0d027fb08966fcc080ba447145
Suguru Miyata <suguru1114@gmail.com>
2024-03-17 15:40:21 +1100

    prop prac

--- src/index.js
+++ src/index.js
@@ -1,33 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card(props) {
+  return (
+    <div className= "my_style"> 
+      <h2>{props.name}</h2>
+      <img
+        src={props.image}
+        alt="avatar_img"
+      />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+{/* 
+      <h2>{props.name2}</h2>
+      <img
+        src={props.image2}
+        alt="tree_img"
+      />
+      <p>{props.tel2}</p>
+      <p>{props.email2}</p> */}
+    
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
+    <Card 
+        name = "Beyounce"    
+        image = "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel = "+123 456 789"
+        email = "b@beyonce.com" 
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
 
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
-    />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
+    <Card 
+        name = "Tree"    
+        image = "https://st2.depositphotos.com/3745557/7435/i/450/depositphotos_74355965-stock-photo-beautiful-oak-at-the-sunset.jpg"
+        tel = "+987 654 321"
+        email = "c@suguru.com" 
+        />
   </div>,
   document.getElementById("root")
 );
